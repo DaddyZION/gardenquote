482bfbecd0838cd6c6f330fc80334d40c09879e4
DaddyZION <kingzionsky@gmail.com>
2026-01-14 16:13:09 +0000

    changed

--- src/components/FeedbackButton.tsx
+++ src/components/FeedbackButton.tsx
@@ -11,7 +11,7 @@ interface FeedbackModalProps {
 }
 
 // Configure your email here
-const FEEDBACK_EMAIL = "xanaval102@atinjo.com";
+const FEEDBACK_EMAIL = "instaquote@pinmx.net";
 
 export function FeedbackModal({ isOpen, onClose }: FeedbackModalProps) {
   const [feedbackType, setFeedbackType] = useState<"bug" | "feature" | "general">("general");
